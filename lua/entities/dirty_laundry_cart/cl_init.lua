ec7109e51fa8873aa77cd6a132848e541a3151fd
Seef0x <47392375+Seef0x@users.noreply.github.com>
2021-12-07 18:59:46 +0100

    Optimization of Cam3D2D

--- lua/entities/dirty_laundry_cart/cl_init.lua
+++ lua/entities/dirty_laundry_cart/cl_init.lua
@@ -1,14 +1,13 @@
 include("shared.lua")
 
+local colWhite = Color(255, 255, 255)
+
 function ENT:Draw()
     self:DrawModel()
 
-    local pos = self:LocalToWorld(self:OBBCenter())
     local ang = self:GetAngles()
 
-    local phrase = string.gsub(LaundryConfig.PhraseDirtyClothes, "<clothes>", self:GetClothesNumber())
-
-    cam.Start3D2D(pos + (ang:Up() * 5) + (ang:Right() * 25), ang + Angle(0, 0, 90), 0.25)
-        draw.SimpleText(tostring(phrase), "DermaLarge", 0, 0, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    cam.Start3D2D(self:LocalToWorld(self:OBBCenter()) + (ang:Up() * 5) + (ang:Right() * 25), ang + Angle(0, 0, 90), 0.25)
+        draw.SimpleText(string.format(LaundryConfig.PhraseDirtyClothes, self:GetClothesNumber()), "DermaLarge", 0, 0, colWhite, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     cam.End3D2D()
 end
--- lua/entities/washing_machine/cl_init.lua
+++ lua/entities/washing_machine/cl_init.lua
@@ -1,24 +1,21 @@
 include("shared.lua")
-include("autorun/vikrlaundry_config.lua")
+
+local colBlack = Color(10, 10, 10, 100)
+local colRed = Color(255, 0, 0)
+local colGreen = Color(0, 255, 0)
 
 function ENT:Draw()
     self:DrawModel()
 
-    local pos = self:LocalToWorld(self:OBBCenter())
     local ang = self:GetAngles()
     ang:RotateAroundAxis(ang:Up(), 90)
     ang:RotateAroundAxis(ang:Forward(), 90)
     
-    local color = Color(255, 0, 0)
     local x, y, w, h = 0, 0, 30, 30
 
-    if self:GetWashing() then
-        color = Color(0, 255, 0)
-    end
-
-    cam.Start3D2D(pos + (ang:Up() * 20.75) + (ang:Forward() * 27.5) + (ang:Right() * 12.5), ang, 0.2)
-        draw.RoundedBox(0, x - w / 2, y - h / 2, w + 2, h + 2, Color(10, 10, 10, 100))
-        draw.RoundedBox(0, x - w / 2 + 1, y - h / 2 + 1, w, h, color)
+    cam.Start3D2D(self:LocalToWorld(self:OBBCenter()) + (ang:Up() * 20.75) + (ang:Forward() * 27.5) + (ang:Right() * 12.5), ang, 0.2)
+        draw.RoundedBox(0, x - w / 2, y - h / 2, w + 2, h + 2, colBlack)
+        draw.RoundedBox(0, x - w / 2 + 1, y - h / 2 + 1, w, h, self:GetWashing() and colGreen or colRed)
     cam.End3D2D()
 end
 
