976068c8297140afab9bf0d5e8e34f8ecc018b12
Seef0x <47392375+Seef0x@users.noreply.github.com>
2022-03-28 23:20:08 +0200

    Optimized draw functions

--- lua/entities/clean_laundry_cart/cl_init.lua
+++ lua/entities/clean_laundry_cart/cl_init.lua
@@ -1,13 +1,16 @@
 include("shared.lua")
 
 local colWhite = Color(255, 255, 255)
+local superAngle = Angle(0, 0, 90)
 
 function ENT:Draw()
     self:DrawModel()
 
+	if self:GetPos():DistToSqr(LocalPlayer():GetPos()) > 500 ^ 2 then return end
+
     local ang = self:GetAngles()
 
-    cam.Start3D2D(self:LocalToWorld(self:OBBCenter()) + (ang:Up() * 5) + (ang:Right() * 21), ang + Angle(0, 0, 90), 0.25)
+    cam.Start3D2D(self:LocalToWorld(self:OBBCenter()) + (ang:Up() * 5) + (ang:Right() * 21), ang + superAngle, 0.25)
         draw.SimpleText(string.format(LaundryConfig.PhraseCleanClothes, self:GetClothesNumber()), "DermaLarge", 0, 0, colWhite, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     cam.End3D2D()
 end
--- lua/entities/washing_machine/cl_init.lua
+++ lua/entities/washing_machine/cl_init.lua
@@ -4,15 +4,17 @@ local colBlack = Color(10, 10, 10, 100)
 local colRed = Color(255, 0, 0)
 local colGreen = Color(0, 255, 0)
 
+local x, y, w, h = 0, 0, 30, 30
+
 function ENT:Draw()
     self:DrawModel()
+
+	if self:GetPos():DistToSqr(LocalPlayer():GetPos()) > 500 ^ 2 then return end
 
     local ang = self:GetAngles()
     ang:RotateAroundAxis(ang:Up(), 90)
     ang:RotateAroundAxis(ang:Forward(), 90)
     
-    local x, y, w, h = 0, 0, 30, 30
-
     cam.Start3D2D(self:LocalToWorld(self:OBBCenter()) + (ang:Up() * 20.75) + (ang:Forward() * 27.5) + (ang:Right() * 12.5), ang, 0.2)
         draw.RoundedBox(0, x - w / 2, y - h / 2, w + 2, h + 2, colBlack)
         draw.RoundedBox(0, x - w / 2 + 1, y - h / 2 + 1, w, h, self:GetWashing() and colGreen or colRed)
